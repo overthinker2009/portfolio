7d3c1e5f481ca6c775254d8473ca83c0568d2e4a
overthinker2009 <164733872+overthinker2009@users.noreply.github.com>
2024-06-11 15:39:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    let dropdownContent = document.getElementById("myDropdown");
+    let button = document.querySelector(".toggle-btn");
+    if (dropdownContent.classList.contains("show")) {
+        dropdownContent.classList.remove("show");
+        button.textContent = "open";
+    } else {
+        dropdownContent.classList.add("show");
+        button.textContent = "close";
+    }
+}
+
